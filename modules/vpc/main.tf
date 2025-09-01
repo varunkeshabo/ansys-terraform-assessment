8c3390a1477858a42c3271c6ed7c98ad5bdc8546
varunkeshabo <varunkeshabo@gmail.com>
2025-08-31 21:38:47 -0400

    add http security group for port 80

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -39,3 +39,21 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
+resource "aws_security_group" "http_sg" {
+  name   = "http-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  }
