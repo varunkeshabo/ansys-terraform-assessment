674857a49c82d5679e6aedc46421ec02f2747b52
varunkeshabo <varunkeshabo@gmail.com>
2025-08-31 21:41:08 -0400

    launch instance in public subnet with http security group

---
+++ modules/ec2/main.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "web" {
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = var.vpc_security_group_ids
+  associate_public_ip_address = true
+  }
